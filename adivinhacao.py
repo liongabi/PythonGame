ce74813f8522b61dc5c9af833bd8347381afdefe
Gabriele Leão Cyganski <92556753+liongabi@users.noreply.github.com>
2022-03-06 19:30:06 -0300

    Add files via upload

---
+++ adivinhacao.py
@@ -0,0 +1,51 @@
+
+def jogar():
+
+    import random
+
+    print("\n*********************************")
+    print("Bem vindo ao jogo de adivinhação!")
+    print("*********************************")
+
+    numero_secreto= random.randrange(1,101)
+    rodada= 1
+    pontos= 1000
+
+    print("\nEscolha o nível de dificuldade: 1. Fácil; 2. Médio; 3. Difícil.")
+    nivel= int(input("Nível selecionado: "))
+
+    if nivel==1:
+        total_tentativas= 50
+    elif nivel==2:
+        total_tentativas= 25
+    elif nivel==3:
+        total_tentativas= 5
+
+    while (rodada <= total_tentativas):
+        print("\nTentativa {} de {}". format(rodada, total_tentativas))
+        chute = int(input("\nDigite o seu número: "))
+        print("\nVocê digitou ", chute)
+
+        acertou = (numero_secreto == chute)
+        errou_menor = (numero_secreto < chute)
+        errou_maior = (numero_secreto > chute)
+
+        if (acertou):
+            print("Você acertou, parabéns!!")
+            break
+        else:
+            if errou_menor:
+                print("Você errou. O número secreto é menor. Tente novamente!")
+            elif errou_maior:
+                print("Você errou. O número secreto é maior. Tente novamente!")
+            pontos_perdidos= abs(numero_secreto - chute)
+            pontos= pontos - pontos_perdidos
+            print("Sua pontuação está em {}". format(pontos))
+        rodada= rodada + 1
+
+    print("\nFim de jogo!")
+    print("Sua pontuação foi {}.".format(pontos))
+    print("\nO número secreto era {}.". format(numero_secreto))
+
+if(__name__ == "__main__"):
+    jogar()
